5c5eefc660b2248f51a68869d7670f5471653493
Lee-Min-Hann <dhany3164@gmail.com>
2025-02-11 11:54:27 +0700

    Create api.php

---
+++ api.php
@@ -0,0 +1,33 @@
+<?php
+
+// use Illuminate\Http\Request;
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\AuthController;
+
+// // Endpoint untuk mengecek status API
+// Route::get('/ping', function () {
+//     return response()->json(['message' => 'API is working!']);
+// });
+// Route::post('auth/login',[AuthController::class,'login']);
+// // Endpoint contoh untuk mengambil daftar pengguna
+// Route::get('/users', function () {
+//     return response()->json([
+//         ['id' => 1, 'name' => 'Mimin', 'email' => 'mimin@example.com'],
+//         ['id' => 2, 'name' => 'John Doe', 'email' => 'john@example.com']
+//     ]);
+// });
+
+// // Endpoint contoh untuk menerima request dengan parameter
+// Route::get('/hello/{name}', function ($name) {
+//     return response()->json(['message' => "Hello, $name!"]);
+// });
+
+// Route::post('/register', [AuthController::class, 'register']);
+
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::post('/logout', [AuthController::class, 'logout']);
+// });
+Route::post('auth/login',[AuthController::class,'login']);
+
